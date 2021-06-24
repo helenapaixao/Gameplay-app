e332e62a7055d5c62283c52f3398172ca413d849
Helena Paixão <hp.helenapaixao@gmail.com>
2021-06-23 22:33:34 -0300

    feat: category select ok

--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,23 +1,30 @@
-import React from 'react';
-import { View} from 'react-native'
-import { styles } from './styles';
+import React, { useEffect, useState } from "react";
+import { View } from "react-native";
+import { styles } from "./styles";
 
-import {Profile} from '../../components/Profile'
-import { ButtonAdd } from '../../components/ButtonAdd';
-import { CategorySelect } from '../../components/CategorySelect';
+import { Profile } from "../../components/Profile";
+import { ButtonAdd } from "../../components/ButtonAdd";
+import { CategorySelect } from "../../components/CategorySelect";
 
 export function Home() {
-  return(
+  const [category, setCategory] = useState("");
 
+  function handleCategorySelect(categoryId: string) {
+    categoryId === category ? setCategory("") : setCategory(categoryId);
+  }
+
+  return (
     <View style={styles.container}>
       <View style={styles.header}>
-        <Profile/>
-        <ButtonAdd/>
+        <Profile />
+        <ButtonAdd />
       </View>
-<View>
-  <CategorySelect/>
-</View>
+      <View>
+        <CategorySelect
+          categorySelected={category}
+          setCategory={handleCategorySelect}
+        />
+      </View>
     </View>
-
-  )
+  );
 }
