84de0653ef81a365aaa4d8d84f05540d92f67280
helenapaixao <hp.helenapaixao@gmail.com>
2022-09-12 15:59:33 -0300

    chore: config project

--- babel.config.js
+++ babel.config.js
@@ -1,6 +1,12 @@
 module.exports = function(api) {
+  plugins: [
+    'react-native-reanimated/plugin'
+  ],
   api.cache(true);
   return {
     presets: ['babel-preset-expo'],
   };
+
 };
+
+module.exports = { presets: ['module:metro-react-native-babel-preset'], plugins: ['react-native-reanimated/plugin'], };
