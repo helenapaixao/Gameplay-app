ec3c8f990a16cfeac2105d06bf6f907583f5507c
helenapaixao <hp.helenapaixao@gmail.com>
2022-09-12 18:01:20 -0300

    fix: config font in category

--- src/components/Category/styles.ts
+++ src/components/Category/styles.ts
@@ -16,35 +16,39 @@ container: {
 content: {
   width: 100,
   height: 116,
-  backgroundColor: theme.colors.secondary40,
   borderRadius: 8,
-  justifyContent: 'space-between',
   alignItems: 'center',
-  paddingVertical: 7
-  
+  justifyContent: 'space-between',
+  paddingVertical: 20,
 },
 title: {
-  fontFamily: theme.fonts.title500,
+  fontFamily: theme.fonts.title700,
   color: theme.colors.heading,
   fontSize: 15
 },
 
 check: {
+  position: 'absolute',
+  top: 7,
+  right: 7,
   width: 12,
   height: 12,
   borderColor: theme.colors.secondary100,
-  alignSelf: 'flex-end',
-  marginRight: 7,
+
+
  backgroundColor: theme.colors.secondary100,
  borderWidth: 2,
  borderRadius:3
 },
 checked: {
+  position: 'absolute',
+  top: 7,
+  right: 7,
   width: 10,
   height: 10,
   borderRadius:3,
-  alignSelf: 'flex-end',
-  marginRight: 7,
+
+
  backgroundColor: theme.colors.primary,
 
 
