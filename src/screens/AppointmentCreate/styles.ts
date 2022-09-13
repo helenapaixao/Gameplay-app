228053d63e360ee224779f17b1b6107af1881c65
helenapaixao <hp.helenapaixao@gmail.com>
2022-09-13 16:07:53 -0300

    feat: add guild, smallinput and button

--- src/screens/AppointmentCreate/styles.ts
+++ src/screens/AppointmentCreate/styles.ts
@@ -1,7 +1,6 @@
 import { StyleSheet } from "react-native";
 import { theme } from "../../global/styles/theme";
 
-
 export const styles = StyleSheet.create({
   container: {
     flex: 1,
@@ -10,34 +9,56 @@ export const styles = StyleSheet.create({
     fontSize: 18,
     fontFamily: theme.fonts.title700,
     color: theme.colors.heading,
-    
   },
   form: {
     paddingHorizontal: 24,
-    marginTop: 32
+    marginTop: 32,
   },
   select: {
-    width: '100%',
+    width: "100%",
     height: 68,
-    flexDirection: 'row',
+    flexDirection: "row",
     borderColor: theme.colors.secondary50,
     borderWidth: 1,
     borderRadius: 8,
-    alignItems: 'center',
+    alignItems: "center",
     paddingRight: 25,
-    overflow: 'hidden'
-
+    overflow: "hidden",
   },
   selectBody: {
     flex: 1,
-    alignItems: 'center',
+    alignItems: "center",
   },
   image: {
     width: 64,
     height: 68,
     backgroundColor: theme.colors.secondary50,
     borderWidth: 1,
-    borderRadius: 8
+    borderRadius: 8,
+  },
+  field: {
+    width: "100%",
+    flexDirection: "row",
+    justifyContent: "space-between",
+    marginTop: 3,
+  },
+  column: {
+    flexDirection: "row",
+    alignItems: "center",
+  },
+  divider: {
+    marginRight: 4,
+    fontSize: 18,
+    color: theme.colors.highlight,
+    fontFamily: theme.fonts.text500,
+  },
+  caracteresLimit: {
+    fontFamily: theme.fonts.text400,
+    fontSize: 13,
+    color: theme.colors.highlight
+  },
+  footer: {
+    marginVertical: 20,
+    marginBottom: 56,
   }
-
-})
+});
